482fdbefcf77a60ce30e8eecddfd8e433ea5750f
eomjaeeun <iyouhe3@naver.com>
2023-12-02 18:01:01 +0900

    Env intersect 수정

--- Code/utils.py
+++ Code/utils.py
@@ -33,30 +33,23 @@ import numpy as np
 def square_intersecting(rectangle_points, origin, direction):
     # 사각형 평면을 정의하는데 사용되는 세 개의 점을 선택합니다.
     p1, p2, p3 = rectangle_points[:3]
+    v1, v2 = p2 - p1, p3 - p1
 
     # 사각형 평면의 법선 벡터를 계산합니다.
-    normal_vector = np.cross(p2 - p1, p3 - p1)
-    normal_vector /= np.linalg.norm(normal_vector)
+    normal = np.cross(v1, v2)
+    normal /= np.linalg.norm(normal)
 
     # 선과 사각형 평면이 평행한지 확인합니다.
-    if np.dot(normal_vector, direction) == 0:
+    if np.dot(normal, direction) == 0:
         return np.inf, False  # 선과 사각형 평면이 평행하면 교차하지 않습니다.
 
     # 선과 사각형 평면이 교차하는 점을 계산합니다.
-    t = np.dot(normal_vector, p1 - origin) / np.dot(normal_vector, direction)
-    intersection_point = origin + t * direction
+    A = np.stack((v1, v2, direction))
+    a, b, k = np.linalg.solve(A, o)
+    intersection_point = a * v1 + b * v2
+    k = -k
 
-    # 교차점이 사각형 내부에 있는지 확인합니다.
-    return is_inside_rectangle(intersection_point, rectangle_points), intersection_point, normal_vector
-
-def is_inside_rectangle(point, rectangle_points):
-    # 사각형의 모든 변의 외적을 계산하여 사각형 내부에 있는지 확인합니다.
-    for i in range(4):
-        p1, p2 = rectangle_points[i], rectangle_points[(i + 1) % 4]
-        edge_vector = p2 - p1
-        normal_vector = np.cross(edge_vector, point - p1)
-
-        if np.dot(normal_vector, p1 - point) < 0:
-            return False  # 교차점이 사각형 외부에 있으면 False 반환
-
-    return True  # 교차점이 사각형 내부에 있으면 True 반환
+    if a < 0 and b < 0:
+        return False, origin, normal
+    else:
+        return True, intersection_point, normal
